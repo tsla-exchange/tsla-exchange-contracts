aac09c8c9371aa8c6b50d6f436fbe0a5efc7d89d
Nick Barry <itsnickbarry@protonmail.ch>
2021-02-13 12:38:28 -0600

    test that no tokens remain in contract

--- test/TSLAExchange.js
+++ test/TSLAExchange.js
@@ -4,11 +4,13 @@ const UNISWAP_ROUTER = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
 const DELEGATE_APPROVALS = '0x15fd6e554874B9e70F832Ed37f231Ac5E142362f';
 const SWAPS = '0xD1602F68CC7C4c7B59D686243EA35a9C73B0c6a2';
 const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const SUSD = '0x57Ab1ec28D129707052df4dF418D58a2D46d5f51';
 const STSLA = '0x918dA91Ccbc32B7a6A0cc4eCd5987bbab6E31e6D';
 
 describe('TSLAExchange', function () {
   let delegateApprovals;
   let usdc;
+  let susd;
   let stsla;
 
   let signer;
@@ -16,6 +18,7 @@ describe('TSLAExchange', function () {
 
   before(async function () {
     usdc = await ethers.getContractAt('IERC20', USDC);
+    susd = await ethers.getContractAt('IERC20', SUSD);
     stsla = await ethers.getContractAt('IERC20', STSLA);
     delegateApprovals = await ethers.getContractAt('IDelegateApprovals', DELEGATE_APPROVALS);
 
@@ -53,10 +56,13 @@ describe('TSLAExchange', function () {
       await usdc.approve(instance.address, ethers.constants.MaxUint256);
       await delegateApprovals.approveExchangeOnBehalf(instance.address);
 
-      const [susd, stsla] = await instance.callStatic.exchange(amount, ethers.constants.Zero);
+      const [susdBalance, stslaBalance] = await instance.callStatic.exchange(
+        amount,
+        ethers.constants.Zero
+      );
 
-      expect(susd).to.equal('986024346725145854844');
-      expect(stsla).to.equal('1208633553837700453');
+      expect(susdBalance).to.equal('986024346725145854844');
+      expect(stslaBalance).to.equal('1208633553837700453');
     });
 
     it('decreases USDC balance of sender', async function () {
@@ -84,6 +90,32 @@ describe('TSLAExchange', function () {
         stsla,
         signer,
         ethers.BigNumber.from('1208631393697978875')
+      );
+    });
+
+    it('leaves no tokens locked in contract', async function () {
+      const amount = ethers.BigNumber.from('1000000000');
+      await usdc.approve(instance.address, ethers.constants.MaxUint256);
+      await delegateApprovals.approveExchangeOnBehalf(instance.address);
+
+      await instance.callStatic.exchange(amount, ethers.constants.Zero);
+
+      expect(
+        await usdc.callStatic.balanceOf(instance.address)
+      ).to.equal(
+        ethers.constants.Zero
+      );
+
+      expect(
+        await susd.callStatic.balanceOf(instance.address)
+      ).to.equal(
+        ethers.constants.Zero
+      );
+
+      expect(
+        await stsla.callStatic.balanceOf(instance.address)
+      ).to.equal(
+        ethers.constants.Zero
       );
     });
 
